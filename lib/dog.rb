a518da0817fc7003eeee5b6184427555da48c4c8
Derek phung <dragevards@gmail.com>
2020-04-22 17:41:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,19 @@
-Class Dog 
+class Dog 
   
+  def name=(dog_name)
+    @name = dog_name
+  end
+  
+  def name
+    @name
+  end
+  
+  def breed=(dog_breed)
+    @breed = dog_breed
+  end
+  
+  def breed
+    @breed
+  end
+  
+end
